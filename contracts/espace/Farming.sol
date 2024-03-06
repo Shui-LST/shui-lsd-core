995ebc62bcec86933c5f78ebaf68e9bc7885cf4b
0xmetaforce <129169179+0xmetaforce@users.noreply.github.com>
2024-03-06 11:36:52 +0800

    Add farming contract and monitor scripts (#1)
    * add monitor script
    
    * polish scfx price monitor error message
    
    * add farming contract
    
    * add doc about voting_escrow and farming
    
    * fix format
    
    ---------
    
    Co-authored-by: 0xmetaforce <0xmetaforce@pm.me>

---
+++ contracts/espace/Farming.sol
@@ -0,0 +1,193 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.18;
+
+import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import {Initializable} from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract Farming is Ownable, Initializable {
+    using EnumerableSet for EnumerableSet.UintSet;
+
+    struct FarmPool {
+        uint256 id;
+        IERC20 stakeToken;
+        IERC20 rewardToken;
+        uint256 rewardPerBlock; // reward rate
+        uint256 startBlock;
+        uint256 endBlock;
+        string name;
+    }
+
+    struct PoolStakeInfo {
+        uint256 lastRewardBlock;
+        uint256 accRewardPerShare;
+        uint256 totalStaked;
+    }
+
+    struct UserStakeInfo {
+        uint256 amount;
+        uint256 userRewardPerTokenPaid;
+        uint256 reward;
+    }
+
+    uint256 public nextPoolId;
+    EnumerableSet.UintSet private poolIds;
+    mapping(uint256 => FarmPool) public poolInfo;
+    mapping(uint256 => PoolStakeInfo) public poolStakeInfo;
+    
+    // user => poolId => UserStakeInfo
+    mapping(address => mapping(uint256 => UserStakeInfo)) public userStakeInfo;
+
+    event UserDeposit(address indexed user, uint256 indexed pid, uint256 amount);
+    event UserWithdraw(address indexed user, uint256 indexed pid, uint256 amount);
+    event UserClaim(address indexed user, uint256 indexed pid, uint256 amount);
+
+    function initialize() public initializer {
+        _transferOwnership(_msgSender());
+    }
+
+    modifier updateReward(address _user, uint256 _pid) {
+        poolStakeInfo[_pid].accRewardPerShare = rewardPerToken(_pid);
+        poolStakeInfo[_pid].lastRewardBlock = lastTimeRewardApplicable(_pid);
+
+        if (_user != address(0)) {
+            userStakeInfo[_user][_pid].reward = reward(_user, _pid);
+            userStakeInfo[_user][_pid].userRewardPerTokenPaid = poolStakeInfo[_pid].accRewardPerShare;
+        }
+
+        _;
+    }
+
+    function deposit(uint256 _pid, uint256 _amount) public updateReward(msg.sender, _pid) {
+        FarmPool memory pool = poolInfo[_pid];
+        require(block.number >= pool.startBlock, "Farming: not started yet");
+        require(block.number <= pool.endBlock, "Farming: already ended");
+        require(_amount > 0, "Farming: invalid amount");
+
+        require(pool.stakeToken.allowance(msg.sender, address(this)) >= _amount, "Farming: not enough allowance");
+        pool.stakeToken.transferFrom(msg.sender, address(this), _amount);
+
+        poolStakeInfo[_pid].totalStaked += _amount;
+        userStakeInfo[msg.sender][_pid].amount += _amount;
+
+        emit UserDeposit(msg.sender, _pid, _amount);
+    }
+
+    function withdraw(uint256 _pid, uint256 _amount) public updateReward(msg.sender, _pid) {
+        require(_amount <= userStakeInfo[msg.sender][_pid].amount, "Farming: not enough balance");
+
+        poolStakeInfo[_pid].totalStaked -= _amount;
+        userStakeInfo[msg.sender][_pid].amount -= _amount;
+
+        FarmPool memory pool = poolInfo[_pid];
+        pool.stakeToken.transfer(msg.sender, _amount);
+
+        emit UserWithdraw(msg.sender, _pid, _amount);
+    }
+
+    function claim(uint256 _pid) public  updateReward(msg.sender, _pid){
+        require(userStakeInfo[msg.sender][_pid].reward > 0, "Farming: no reward");
+        
+        uint256 _reward = userStakeInfo[msg.sender][_pid].reward;
+        userStakeInfo[msg.sender][_pid].reward = 0;
+
+        poolInfo[_pid].rewardToken.transfer(msg.sender, _reward);
+
+        emit UserClaim(msg.sender, _pid, _reward);
+    }
+
+    function reward(address _user, uint256 _pid) public view returns (uint256) {
+        UserStakeInfo memory sInfo = userStakeInfo[_user][_pid];
+        return (
+            (
+                sInfo.amount * (rewardPerToken(_pid) - sInfo.userRewardPerTokenPaid)
+            ) / 1e18
+        ) + sInfo.reward;
+    }
+
+    function poolLength() public view returns (uint256) {
+        return poolIds.length();
+    }
+
+    function poolIdsAt(uint256 _index) public view returns (uint256) {
+        return poolIds.at(_index);
+    }
+
+    function getPools() public view returns (FarmPool[] memory) {
+        uint256 _length = poolLength();
+        FarmPool[] memory _pools = new FarmPool[](_length);
+        for (uint256 i = 0; i < _length; i++) {
+            _pools[i] = poolInfo[poolIdsAt(i)];
+        }
+        return _pools;
+    }
+
+    function getPools(uint256 skip, uint256 limit) public view returns (FarmPool[] memory) {
+        uint256 _length = poolLength();
+        require(skip < _length, "Farming: invalid skip");
+        uint256 _end = (skip + limit) > _length ? _length : (skip + limit);
+        FarmPool[] memory _pools = new FarmPool[](_end - skip);
+        for (uint256 i = skip; i < _end; i++) {
+            _pools[i - skip] = poolInfo[poolIdsAt(i)];
+        }
+        return _pools;
+    }
+
+    function addPool(
+        address _stakeToken,
+        address _rewardToken,
+        uint256 _totalReward,
+        uint256 _startBlock,
+        uint256 _endBlock,
+        string memory _name
+    ) public onlyOwner {
+        require(_startBlock > block.number, "Farming: invalid start block");
+        require(_endBlock > _startBlock, "Farming: invalid end block");
+
+        require(IERC20(_rewardToken).allowance(_msgSender(), address(this)) >= _totalReward, "Farming: not enough allowance");
+        IERC20(_rewardToken).transferFrom(_msgSender(), address(this), _totalReward);
+        
+        uint256 _pid = nextPoolId++;
+        uint256 _rewardPerBlock = _totalReward / (_endBlock - _startBlock);
+
+        poolInfo[_pid] = FarmPool({
+            id: _pid,
+            stakeToken: IERC20(_stakeToken),
+            rewardToken: IERC20(_rewardToken),
+            rewardPerBlock: _rewardPerBlock,
+            startBlock: _startBlock,
+            endBlock: _endBlock,
+            name: _name
+        });
+
+        poolIds.add(_pid);
+
+        poolStakeInfo[_pid] = PoolStakeInfo({
+            lastRewardBlock: _startBlock,
+            accRewardPerShare: 0,
+            totalStaked: 0
+        });
+    }
+
+    function lastTimeRewardApplicable(uint256 _pid) public view returns (uint256) {
+        return _min(poolInfo[_pid].endBlock, block.number);
+    }
+
+    function rewardPerToken(uint256 _pid) public view returns (uint256) {
+        PoolStakeInfo memory pStakeInfo = poolStakeInfo[_pid];
+        if (pStakeInfo.totalStaked == 0) {
+            return pStakeInfo.accRewardPerShare;
+        }
+
+        FarmPool memory pool = poolInfo[_pid];
+
+        return pStakeInfo.accRewardPerShare
+            + (pool.rewardPerBlock * (lastTimeRewardApplicable(_pid) - pStakeInfo.lastRewardBlock) * 1e18)
+                / pStakeInfo.totalStaked;
+    }
+
+    function _min(uint256 x, uint256 y) private pure returns (uint256) {
+        return x <= y ? x : y;
+    }
+}
---
+++ forgeTests/Farming.t.sol
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.18;
+
+import "forge-std/Test.sol";
+import "../contracts/espace/Farming.sol";
+import "../contracts/utils/Proxy1967.sol";
+import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
+
+contract FarmingTest is Test {
+
+    address public user1 = address(1111);
+    address public user2 = address(1112);
+
+    ERC20PresetMinterPauser public stakeToken;
+    ERC20PresetMinterPauser public rewardToken;
+
+    Farming public farming;
+
+    function setUp() public {
+        stakeToken = new ERC20PresetMinterPauser("StakeToken", "ST");
+        stakeToken.mint(user1, 1000_000_000 ether);
+        stakeToken.mint(user2, 1000_000_000 ether);
+
+        rewardToken = new ERC20PresetMinterPauser("RewardToken", "RT");
+        rewardToken.mint(user2, 1000_000_000 ether);
+
+        Farming impl = new Farming();
+        Proxy1967 p1 = new Proxy1967(address(impl), abi.encodeWithSignature("initialize()"));
+        farming = Farming(address(p1));
+
+        rewardToken.mint(farming.owner(), 1000_000_000 ether);
+        // console.log(farming.owner());
+
+        rewardToken.approve(address(farming), 1000_000_000 ether);
+        farming.addPool(address(stakeToken), address(rewardToken), 1000 ether, 1000, 2000, "test pool");
+        
+        vm.deal(user1, 1000_000_000 ether);
+        vm.deal(user2, 1000_000_000 ether);
+    }
+
+    function test_deposit() public {
+        assertEq(farming.poolLength(), 1);
+
+        uint256 _pid = 0;
+        uint256 startBlock = 1000;
+
+        vm.startPrank(user1);
+        stakeToken.approve(address(farming), 1000 ether);
+
+        vm.roll(startBlock);
+        farming.deposit(_pid, 1000 ether);
+        
+        (uint amount, uint rewardPerToken, uint reward) = farming.userStakeInfo(user1, _pid);
+        assertEq(amount, 1000 ether);
+        // console.log(rewardPerToken, reward);
+        
+        vm.roll(startBlock + 1);
+
+        (uint lastBlock, uint accReward, uint totalStake) = farming.poolStakeInfo(_pid);
+        assertEq(lastBlock, 1000);
+        assertEq(totalStake, 1000 ether);
+        
+
+        assertEq(farming.reward(user1, _pid), 1 ether);
+
+        vm.roll(startBlock + 2);
+        assertEq(farming.reward(user1, _pid), 2 ether);
+
+        farming.claim(_pid);
+        assertEq(farming.reward(user1, _pid), 0);
+
+        vm.roll(startBlock + 3);
+        assertEq(farming.reward(user1, _pid), 1 ether);
+
+        vm.stopPrank();
+
+        vm.startPrank(user2);
+        stakeToken.approve(address(farming), 1000 ether);
+
+        farming.deposit(_pid, 1000 ether);
+
+        vm.roll(startBlock + 4);
+        assertEq(farming.reward(user2, _pid), 0.5 ether);
+
+        vm.roll(startBlock + 1000);
+        assertEq(farming.reward(user2, _pid), 997 ether / 2);
+
+        vm.roll(startBlock + 1005);
+        assertEq(farming.reward(user2, _pid), 997 ether / 2);
+
+        farming.withdraw(_pid, 999 ether);
+        (uint amount1, ,) = farming.userStakeInfo(user2, _pid);
+        assertEq(amount1, 1 ether);
+
+        vm.stopPrank();
+    }
+}
